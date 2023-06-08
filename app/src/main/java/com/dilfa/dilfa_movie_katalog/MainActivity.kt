0e1fafb26507d09c2e614118ddf9a6e0d15e2810
AfifAdhitama <oskadonpengsus44@gmail.com>
2023-06-08 14:01:30 +0700

    Initial commit

---
+++ app/src/main/java/com/dilfa/dilfa_movie_katalog/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dilfa.dilfa_movie_katalog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/dilfa/dilfa_movie_katalog/TVDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.dilfa.dilfa_movie_katalog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TVDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tvdetail)
+    }
+}
